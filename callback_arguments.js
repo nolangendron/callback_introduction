00ce54f75ea1c6ad7796b848e62fe0fc7dece949
Nolan <nolanglengendron@gmail.com>
2019-05-30 18:31:21 +0000

    added forEach

--- callback_arguments.js
+++ callback_arguments.js
@@ -1,10 +1,15 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(arr[i], i);   // execute callback
-    }
-  }
+  arr.forEach(
+    function(element, index) {
+    if (element === "Waldo")
+    found(element, index)
+    });
+  // for (var i = 0; i < arr.length; i++) {
+  //   if (arr[i] === "Waldo") {
+  //     found(arr[i], i);   // execute callback
+  //   }
+  // }
 }
 
 function actionWhenFound(name, index) {
